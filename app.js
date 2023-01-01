6fb5b43115aa0c72a410f25dd35e9541ffca42ab
FedeSebas <federicoarce2004@gmail.com>
2023-01-01 11:44:48 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+
+const app = express();
+
+app.set("view engine","ejs");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+
+const items = ["Buy Milk","Do Homework","Do tasks"];
+const workItems = [];
+
+
+
+app.get("/",function(req,res){
+  let day = date.getDate();
+  res.render("list",{listTitle: day, itemsOutput: items});
+})
+
+app.get("/work",function(req,res){
+  res.render("list",{listTitle: "Work List",itemsOutput: workItems});
+})
+
+app.post("/",function(req,res){
+  let item = req.body.item;
+  if(req.body.list == "Work List"){
+    workItems.push(item);
+    res.redirect("/work");
+  }
+  else{
+    items.push(item);
+    res.redirect("/");
+  }
+})
+
+app.get("/about",function(req,res){
+  res.render("about")
+})
+
+app.listen(3000,function(){
+  console.log("Running on port 3000");
+})
+
