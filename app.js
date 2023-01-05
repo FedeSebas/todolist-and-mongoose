e8f609fdc5cac0aabd29c15055776351b5d46315
FedeSebas <federicoarce2004@gmail.com>
2023-01-05 12:07:50 -0300

    another commit

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
+const _ = require("lodash");
+
 
 const app = express();
 
@@ -9,31 +11,63 @@ app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+mongoose.set('strictQuery', true);
 
-const items = ["Buy Milk","Do Homework","Do tasks"];
-const workItems = [];
+mongoose.connect("mongodb://192.168.0.110:27017/todolistDB2",{useNewUrlParser: true});
+
+const itemSchema = new mongoose.Schema({
+  name: String
+})
+
+const Item = mongoose.model("Item",itemSchema);
+
+const item1 = new Item({
+  name: "<--- Click the check button to deletem an item"
+})
+
+const item2 = new Item({
+  name: "Add content in the input"
+})
+
+const item3 = new Item({
+  name: "Add an item clicking the + symbol"
+})
+
+const defaultItems = [item1,item2,item3]
+
+const listSchema = new mongoose.Schema({
+  name: String,
+  items: [itemSchema]
+})
+
+const List = mongoose.model("List",listSchema);
 
 
 
 app.get("/",function(req,res){
-  let day = date.getDate();
-  res.render("list",{listTitle: day, itemsOutput: items});
+  Item.find({},function(err,foundItems){
+    if(err)console.log(err);
+    else{
+      if(foundItems.length === 0){
+        Item.insertMany(defaultItems,function(err){
+          if(err)console.log();
+          else console.log("Successfully saved in the collection!");
+        })
+        res.redirect("/")
+      }else{
+        console.log(foundItems)
+        res.render("list",{listTitle: "Today", itemsOutput: foundItems});
+      }
+    }
+  })
 })
 
 app.get("/work",function(req,res){
-  res.render("list",{listTitle: "Work List",itemsOutput: workItems});
+  res.render("list",{listTitle: "Work List",itemsOutput: ["hi","dont touch that"]});
 })
 
 app.post("/",function(req,res){
-  let item = req.body.item;
-  if(req.body.list == "Work List"){
-    workItems.push(item);
-    res.redirect("/work");
-  }
-  else{
-    items.push(item);
-    res.redirect("/");
-  }
+  
 })
 
 app.get("/about",function(req,res){
